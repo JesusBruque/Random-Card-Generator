f078c40035e3df3358aedd671524d26d0daec8dc
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-01-08 17:50:59 +0000

    Ramdon Card

---
+++ app.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+
+    let suits = ["♦", "♥", "♠", "♣"];
+
+    let randomSuitsNumber = Math.floor(Math.random() * 4);
+    let number = Math.floor(Math.random() * (13 - 1) + 1);
+    let randomSuits = suits[randomSuitsNumber];
+    const card = document.querySelector(".card");
+
+    if(number == 10){
+        number = "J";
+    } else if (number == 11){
+        number = "Q";
+    } else if (number == 12){
+        number = "K";
+    }
+
+    card.innerHTML = `
+                        <div class="${randomSuits} col-12 text-start">${randomSuits}</div>
+                        <div class="${number} col-12-auto text-center">${number}</div>
+                        <div class="${randomSuits} col-12 text-end">${randomSuits}</div>
+                     `;
+}
+
+setTimeout('document.location.reload()',10000);
